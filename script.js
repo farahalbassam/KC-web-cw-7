88afee9de4620f95ef053648168570a1e3273944
farah albassam <1402@rawdalsaleheen.edu.kw>
2022-08-22 00:05:21 +0300

    cw 7 done

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+function BMI(weigth,height) {
+
+    let result = weigth / (height * height)
+    return result;
+}
+
+console.log(BMI(70,1.7))
+
+function stutus(bmi){
+if (bmi< 18.5){
+    return ("you are below weigth")
+} else if (bmi >= 18.5 && bmi<25){
+    return ("you are in healthy weigth")
+} else {
+    ("you are over weigth")
+}
+
+}
+
+function calculate (){
+    let weigth = document.getElementById("weigth").value;
+
+    let height = document.getElementById("hieght").value;
+
+    let bmi = BMI(weigth,height);
+
+    let desc = stutus(bmi);
+
+    let div = document.getElementById("result");
+    
+    div.innerText = bmi + " == " + desc
+}
 
